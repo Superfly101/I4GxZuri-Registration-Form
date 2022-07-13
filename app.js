31096d2707e90be1c30d0c5f52328cd533c1df6f
Superfly <danielukoha101@gmail.com>
2022-07-13 11:23:35 +0100

    updating form logic

--- app.js
+++ app.js
@@ -21,7 +21,17 @@ inputs.forEach((input) => {
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  validateInput();
+  inputs.forEach((input) => {
+    if (input.type === "email") {
+      if (input.value === "" || !isValid(input.value)) {
+        showError(input);
+      }
+    } else {
+      if (input.value === "") {
+        showError(input);
+      }
+    }
+  });
   const errorInput = document.querySelector(".error input:first-child");
   if (errorInput) {
     document.querySelector(".error input:first-child").focus();
@@ -44,35 +54,4 @@ function isValid(email) {
   return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
     email
   );
-}
-
-function validateInput() {
-  const firstNameValue = firstNameInput.value;
-  const lastNameValue = lastNameInput.value;
-  const emailValue = emailAddressInput.value;
-  const passwordValue = passwordInput.value;
-
-  if (firstNameValue.trim() === "") {
-    showError(firstNameInput);
-  } else {
-    clearError(firstNameInput);
-  }
-
-  if (lastNameValue.trim() === "") {
-    showError(lastNameInput);
-  } else {
-    clearError(lastNameInput);
-  }
-
-  if (emailValue.trim() === "" || !isValid(emailValue)) {
-    showError(emailAddressInput);
-  } else {
-    clearError(emailAddressInput);
-  }
-
-  if (passwordValue === "") {
-    showError(passwordInput);
-  } else {
-    clearError(passwordInput);
-  }
 }
