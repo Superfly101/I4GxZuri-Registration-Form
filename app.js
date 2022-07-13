6aacc016bccf5b9832e4abef4276e0be3f0b505a
Superfly <danielukoha101@gmail.com>
2022-07-13 10:49:52 +0100

    updating form input validation

--- app.js
+++ app.js
@@ -2,17 +2,32 @@ const firstNameInput = document.querySelector("#first-name");
 const lastNameInput = document.querySelector("#last-name");
 const emailAddressInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
+const inputs = document.querySelectorAll("input");
 
 const form = document.querySelector(".form");
+
+const inputHandler = (event) => {
+  if (event.target.value.trim() === "") {
+    showError(event.target);
+  } else {
+    clearError(event.target);
+  }
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("blur", inputHandler);
+  input.addEventListener("input", inputHandler);
+});
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   validateInput();
-  const errorInput = document.querySelector('.error input:first-child');
+  const errorInput = document.querySelector(".error input:first-child");
   if (errorInput) {
-    document.querySelector('.error input:first-child').focus()
+    document.querySelector(".error input:first-child").focus();
+    return;
   }
-
+  inputs.forEach((input) => (input.value = ""));
 });
 
 function showError(input) {
@@ -26,7 +41,9 @@ function clearError(input) {
 }
 
 function isValid(email) {
-    return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+  return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
 }
 
 function validateInput() {
